528b3d09d174b652fe8b6fc940ed3f1b66e638d5
DaraHds <D00278076@student.dkit.ie>
2025-10-08 15:21:47 +0100

    !!!

--- Halloween Horrors/Assets/Scripts/Zombie.cs
+++ Halloween Horrors/Assets/Scripts/Zombie.cs
@@ -5,31 +5,81 @@ using UnityEngine;
 public class Zombie : MonoBehaviour
 {
     Animator _animator;
-    int direction=1;
+    int direction = 1;
     float timeInDirection;
 
     public float distanceTime;
     public float speed;
-
+    public int health;
+    float dieTime = 1;
+    bool isDead = false;
+    bool isIdle = false;
+    public float idleTime = 2;
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
         timeInDirection = distanceTime;
+        _animator.SetFloat("Move X",direction);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timeInDirection < 0)
+        if (!isDead)
         {
-            direction = direction * -1;
-            timeInDirection = distanceTime;
+            if (isIdle && idleTime < 0)
+            {
+                direction = direction * -1;
+                _animator.SetInteger("Direction", direction);
+                _animator.SetFloat("Move", 1);
+                timeInDirection = distanceTime;
+                isIdle = false;
+            }
+            else if (!isIdle && timeInDirection < 0)
+            {
+                idleTime = 2;
+                isIdle=true;
+                _animator.SetFloat("Move", 0);
+
+            }
+            if (!isIdle)
+            {
+                Vector2 pos = transform.position;
+                pos.x = pos.x + (speed * Time.deltaTime * direction);
+                transform.position = pos;
+                timeInDirection -= Time.deltaTime;
+            }
+            else 
+            {
+                idleTime -= Time.deltaTime;
+            }
         }
-        Vector2 pos = transform.position;
-        pos.x = pos.x + (speed * Time.deltaTime *direction);
-        transform.position = pos;
-        timeInDirection -= Time.deltaTime;
+        
+        else
+        {
+            dieTime -= Time.deltaTime;
+            if (dieTime < 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "PlayerProjectile")
+        {
+            health--;
+            if (health <= 0)
+            {
+                isDead = true;
+                _animator.SetBool("Dead", true);
+            }
+        }
+    }
+
+    
+
 }
